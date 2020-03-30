bb4539ffa09a75ffe12fb50375b43cb99d0f9cc0
Igor Risteski <pirej30@yahoo.com>
2020-03-30 16:40:59 +0200

    Initial setup

---
+++ index.js
@@ -0,0 +1,40 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButtton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButtton = pauseButtton;
+
+        this.durationInput.addEventListener('value', this.duration);
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButtton.addEventListener('click', this.pause);
+    }
+    start = () => {
+        this.tick();  // call it once, just to start ticking right away
+        this.interval = setInterval(this.tick, 1000); // setInterval is builtin JS method
+        // clearInterval(timer);
+    };
+    pause = () => {
+        clearInterval(this.interval);
+        console.log('Paused!');
+    };
+
+    // duration = () => {
+    //     console.log('Did you set a starting number?');
+
+    // };
+    tick = () => {
+        console.log('tick');
+        const timeRemaining = parseFloat(this.durationInput.value);
+        this.durationInput.value = timeRemaining - 1;
+
+    };
+
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButtton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButtton);
+
+
