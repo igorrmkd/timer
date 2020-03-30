690a6d692a135bc86942f91210a32d2a1a38cad5
Igor Risteski <pirej30@yahoo.com>
2020-03-30 16:55:26 +0200

    use setter

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ class Timer {
     tick = () => {
         console.log('tick');
         const timeRemaining = this.timeRemaining;
-        this.durationInput.value = timeRemaining - 1;
+        this.timeRemaining = timeRemaining - 1;
     };
 
     get timeRemaining() {
         return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
     }
 
 }
